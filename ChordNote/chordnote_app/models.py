5098efcc0af09c1bdf888a06ed4816dbb50ef5ce
MrCRMing <1127125637@qq.com>
2019-08-07 14:45:42 +0800

    no message

--- ChordNote/chordnote_app/models.py
+++ ChordNote/chordnote_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Users(models.Model):
     class Meta:
@@ -11,7 +12,15 @@ class Users(models.Model):
 
     # 指定外键
 
-
     def __str__(self):
         return "%s-%s-%s" % (self.email, self.name, self.password)
 
+
+class checkcode(models.Model):
+    class Meta:
+        db_table = "checkcode"
+
+    email = models.CharField(max_length=30,primary_key=True)
+    code = models.CharField(max_length=10)
+    create_time = models.DateTimeField(auto_now_add=True, null=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
